7bd69a3750117f341bb83627ac8e42646340dcb1
alejandro.fernandez <alex11_campo@hotmail.com>
2013-11-13 18:29:21 +0000

    Añadidos nuevos DAOs

---
+++ PracticaMaD/trunk/Model/RecommendationDao/IRecommendationDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.RecommendationDao
+{
+    public interface IRecommendationDao : IGenericDao<Recommendation, Int64>
+    {
+    }
+}
