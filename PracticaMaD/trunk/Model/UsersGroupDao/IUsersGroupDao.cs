c37cba7c9547a263992b5cc1b9b8010eaeb9db1a
elias.grande <elias.grande@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-11-13 19:03:26 +0000

    Añadidas interfaces y clases de daos vacías que faltaban y definida la interfaz IUserGroupDao.

--- PracticaMaD/trunk/Model/UsersGroupDao/IUsersGroupDao.cs
+++ PracticaMaD/trunk/Model/UsersGroupDao/IUsersGroupDao.cs
@@ -8,6 +8,21 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.UsersGroupDao
 {
     public interface IUsersGroupDao : IGenericDao<UsersGroup, Int64>
     {
+        /// <summary>
+        /// Returns a list of groups which the user is member of. If the user
+        /// is not member of any group, an empty list is returned.
+        /// </summary>
+        /// <param name="userId">the user identifier</param>
+        /// <param name="startIndex">the index of the first account to return (starting in 0)</param>
+        /// <param name="count">the maximum number of accounts to return</param>
+        /// <returns>the list of accounts</returns>
+        List<UsersGroup> FindByUserId(long userId, int startIndex, int count);
 
+        /// <summary>
+        /// Returns the number of groups which the user is member of.
+        /// </summary>
+        /// <param name="userId">the user identifier</param>
+        /// <returns>the number of groups</returns>
+        int GetNumberOfUserGroups(long userId);
     }
 }
