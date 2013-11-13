3b6ba66ab4d2200ab18b8e35ec233fe2d32a9e16
alejandro.fortes.lopes <alejandro.fortes.lopes@gmail.com>
2013-11-13 18:34:24 +0000

    Añadido el parametro id en la operacion register del UserService

---
+++ PracticaMaD/trunk/Model/CommentDao/ICommentDao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
+{
+    public interface ICommentDao : IGenericDao<Comment, Int64>
+    {
+        /// <summary>
+        /// Finds the by event identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="startIndex">The start index.</param>
+        /// <param name="count">The count.</param>
+        /// <returns></returns>
+        List<Comment> FindByEventId(long id, int startIndex, int count);
+    }
+}
