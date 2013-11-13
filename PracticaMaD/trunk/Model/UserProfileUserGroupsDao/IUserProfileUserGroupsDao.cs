a80ac57028c7a06e3a2a490a7911ad3c34ca546a
elias.grande <elias.grande.casedas@gmail.com>
2013-11-13 19:03:26 +0000

    Añadidas interfaces y clases de daos vacías que faltaban y definida la interfaz IUserGroupDao.

---
+++ PracticaMaD/trunk/Model/UserProfileUserGroupsDao/IUserProfileUserGroupsDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model
+{
+    interface IUserProfileUserGroupsDao
+    {
+    }
+}
