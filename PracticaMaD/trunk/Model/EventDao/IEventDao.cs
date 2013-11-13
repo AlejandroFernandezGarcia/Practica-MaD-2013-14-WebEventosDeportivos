d849498144e7b334485d5686abe04c7007a32f16
alejandro.fernandez <alejandro.fernandez@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-11-13 16:43:24 +0000

    Añadidos los DAOs para Category y Event

---
+++ PracticaMaD/trunk/Model/EventDao/IEventDao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.EventDao
+{
+    public interface IEventDao : IGenericDao<Event, Int64>
+    {
+        List<Event> FindByKeywords(String keywords, int categoryId);
+    }
+}
