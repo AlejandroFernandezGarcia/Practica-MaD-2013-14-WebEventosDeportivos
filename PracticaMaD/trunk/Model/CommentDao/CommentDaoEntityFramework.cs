84712e7e5545be920cf4436e5e441861c4066622
alejandro.fortes.lopes <alejandro.fortes.lopes@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-11-13 18:36:59 +0000

    Añadido CommentDaoEntityFramework. Esta vacio

---
+++ PracticaMaD/trunk/Model/CommentDao/CommentDaoEntityFramework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
+{
+    class CommentDaoEntityFramework : GenericDaoEntityFramework<Comment, Int64>, ICommentDao
+    {
+        public List<Comment> FindByEventId(long id, int startIndex, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
