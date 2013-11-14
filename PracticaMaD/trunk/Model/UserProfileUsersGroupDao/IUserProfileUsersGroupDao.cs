b139fc67e542dd1fef71e955116d1463521b1d74
alejandro.fernandez <alex11_campo@hotmail.com>
2013-11-14 01:38:53 +0000

    Creados, modificados y arreglados los DAOs Creados, modificados y arreglados los Service (falta por implementar EventService y RecommendationService) Modificado el script de SQL, se le han añadido algunas restricciones. Modificado el .edmx habia algunos errores tipográficos en los nombres de las entidades y tablas.
    Casi seguro falta algún método en los DAOs.

---
+++ PracticaMaD/trunk/Model/UserProfileUsersGroupDao/IUserProfileUsersGroupDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.UserProfileUsersGroupDao
+{
+    public interface IUserProfileUsersGroupDao : IGenericDao<UserProfileUsersGroup,Int64>
+    {
+        void RemoveUserFromGroup(long usersGroupId, long userProfileId);
+
+        void AddUserToGroup(UserProfileUsersGroup userProfileUsersGroup);
+
+        UserProfileUsersGroup FindByUserIdAndGroupId(long usersGroupId, long userProfileId);
+    }
+}
