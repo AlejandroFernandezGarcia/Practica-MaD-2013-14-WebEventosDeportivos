22a701919cfd4d838ae07c7e3964b0020d094104
alejandro.fernandez <alex11_campo@hotmail.com>
2013-11-14 12:26:34 +0000

    Acabados servicios y DAOs

--- PracticaMaD/trunk/Model/EventService/EventService.cs
+++ PracticaMaD/trunk/Model/EventService/EventService.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Practices.Unity;
+using Es.Udc.DotNet.PracticaMaD.Model.EventDao;
+using Es.Udc.DotNet.PracticaMaD.Model.CommentDao;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.EventService
 {
     class EventService : IEventService
     {
+        [Dependency]
+        public IEventDao EventDao { private get; set; }
+
+        [Dependency]
+        public ICommentDao CommentDao { private get; set; }
+
         public List<Event> FindByKeywords(string keywords, Category category)
         {
-            throw new NotImplementedException();
+            if (category != null)
+            {
+                return EventDao.FindByKeywords(keywords, category.id);
+            }
+            else 
+            {
+                return EventDao.FindByKeywords(keywords, -1);
+            }
+
         }
 
         public void AddComment(Event even, string text, UserProfile userProfile)
         {
-            throw new NotImplementedException();
+            DateTime date = new DateTime();
+            byte[] dateBytes = BitConverter.GetBytes(date.Ticks);
+
+            Comment comment = Comment.CreateComment(0,dateBytes,text,even.id,userProfile.id);
+
+            CommentDao.Create(comment);
         }
     }
 }
