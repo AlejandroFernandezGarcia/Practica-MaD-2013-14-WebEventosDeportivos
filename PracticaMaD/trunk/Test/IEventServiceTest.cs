bdba0c370dff7c6bddcef8d4026c58ec5fc00881
alejandro.fernandez <alejandro.fernandez@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-11-16 12:54:01 +0000

    Generadas tablas de nuevo con la columna contador bien configurada. Generado .edmx a partir de las tablas. Arreglado error de consulta en el UserProfileDaoEntityFramework.
    Añadido nuevo test pero vacío.

---
+++ PracticaMaD/trunk/Test/IEventServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Es.Udc.DotNet.PracticaMaD.Test
+{
+
+    [TestClass]
+    public class IEventServiceTest
+    {
+
+    }
+}
