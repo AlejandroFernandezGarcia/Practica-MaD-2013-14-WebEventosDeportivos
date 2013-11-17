a8bbd704e9dafe125564a10667a323332324c4d8
alejandro.fernandez <alex11_campo@hotmail.com>
2013-11-17 13:32:27 +0000

    Arreglada consultas en los daos. Arreglado metodo en el service.
    Añadidos test dentro de la clase EventServiceTest.
    
    Añadidas clases para los demás test.

---
+++ PracticaMaD/trunk/Test/IUsersGroupServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Es.Udc.DotNet.PracticaMaD.Test
+{
+    /// <summary>
+    /// Descripción resumida de IUsersGroupServiceTest
+    /// </summary>
+    [TestClass]
+    public class IUsersGroupServiceTest
+    {
+       
+    }
+}
