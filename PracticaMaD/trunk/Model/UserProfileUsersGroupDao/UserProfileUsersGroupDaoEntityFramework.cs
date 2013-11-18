866e987cf04d36cc6a9a76a8000af143889dd12b
elias.grande <elias.grande.casedas@gmail.com>
2013-11-18 03:02:42 +0000

    Todos los test arreglados, era por culpa de las delicious copy-pastas, en el FROM del query de FindByUserIdAndGroupId ponía "UserProfileUsersGroupDao" en vez de "UserProfileUsersGroup".

--- PracticaMaD/trunk/Model/UserProfileUsersGroupDao/UserProfileUsersGroupDaoEntityFramework.cs
+++ PracticaMaD/trunk/Model/UserProfileUsersGroupDao/UserProfileUsersGroupDaoEntityFramework.cs
@@ -27,20 +27,14 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.UserProfileUsersGroupDao
         {
             UserProfileUsersGroup upug = null;
             
-            String query = "SELECT VALUE e FROM PracticaMaDEntities.UserProfileUsersGroupDao AS e " +
+            String query = "SELECT VALUE e FROM PracticaMaDEntities.UserProfileUsersGroup AS e " +
                            "WHERE e.userId = @userProfileId " +
                            "AND e.groupID = @usersGroupId ";
 
             ObjectParameter param = new ObjectParameter("userProfileId", userProfileId);
             ObjectParameter param2 = new ObjectParameter("usersGroupId", usersGroupId);
 
-            ObjectQuery<UserProfileUsersGroup> oQuery = this.Context.CreateQuery<UserProfileUsersGroup>(query, param,
-                                                                                                        param2);
-            // el test falla aquí, si descomentas esta excepción la verás:
-            //throw new Exception("Antes del Execute");
-            ObjectResult<UserProfileUsersGroup> result = oQuery.Execute(MergeOption.AppendOnly);
-            // pero si solo descomentas esta no la verás:
-            //throw new Exception("Después del Execute");
+            ObjectResult<UserProfileUsersGroup> result = this.Context.CreateQuery<UserProfileUsersGroup>(query, param, param2).Execute(MergeOption.AppendOnly);
 
             try
             {
