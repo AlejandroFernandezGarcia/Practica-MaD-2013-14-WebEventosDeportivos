6acc2872670eb5958ffb3e126ef1ff5a37836d23
alejandro.fernandez <alejandro.fernandez@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-11-30 12:31:49 +0000

    Formateado y añadidos comentarios al proyecto test.

--- PracticaMaD/trunk/Test/IRecommendationServiceTest.cs
+++ PracticaMaD/trunk/Test/IRecommendationServiceTest.cs
@@ -1,101 +1,88 @@
-﻿//using System;
-//using System.Text;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Microsoft.Practices.Unity;
-//using Es.Udc.DotNet.PracticaMaD.Model.RecommendationDao;
-//using Es.Udc.DotNet.PracticaMaD.Model.RecommendationService;
-//using System.Transactions;
-//using Es.Udc.DotNet.PracticaMaD.Model.EventDao;
-//using Es.Udc.DotNet.PracticaMaD.Model.UsersGroupDao;
-//using Es.Udc.DotNet.PracticaMaD.Model;
-//using Es.Udc.DotNet.PracticaMaD.Model.CategoryDao;
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Practices.Unity;
+using Es.Udc.DotNet.PracticaMaD.Model.RecommendationDao;
+using Es.Udc.DotNet.PracticaMaD.Model.RecommendationService;
+using System.Transactions;
+using Es.Udc.DotNet.PracticaMaD.Model.EventDao;
+using Es.Udc.DotNet.PracticaMaD.Model.UsersGroupDao;
+using Es.Udc.DotNet.PracticaMaD.Model;
+using Es.Udc.DotNet.PracticaMaD.Model.CategoryDao;
 
-//namespace Es.Udc.DotNet.PracticaMaD.Test
-//{
-//    /// <summary>
-//    /// Descripción resumida de IRecommendationServiceTest
-//    /// </summary>
-//    [TestClass]
-//    public class IRecommendationServiceTest
-//    {
-//        private static IUnityContainer container;
-//        private static IRecommendationService recommendationService;
-//        private static IRecommendationDao recommendationDao;
-//        private static IEventDao eventDao;
-//        private static IUsersGroupDao usersGroupDao;
-//        private static ICategoryDao categoryDao;
+namespace Es.Udc.DotNet.PracticaMaD.Test
+{
+    /// <summary>
+    /// Descripción resumida de IRecommendationServiceTest
+    /// </summary>
+    [TestClass]
+    public class IRecommendationServiceTest
+    {
+        private static IUnityContainer container;
+        private static IRecommendationService recommendationService;
+        private static IRecommendationDao recommendationDao;
+        private static IEventDao eventDao;
+        private static IUsersGroupDao usersGroupDao;
+        private static ICategoryDao categoryDao;
 
-//        TransactionScope transaction;
+        TransactionScope transaction;
 
-//        private TestContext testContextInstance;
+        private TestContext testContextInstance;
 
-//        public TestContext TestContext
-//        {
-//            get
-//            {
-//                return testContextInstance;
-//            }
-//            set
-//            {
-//                testContextInstance = value;
-//            }
-//        }
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
 
-//        [ClassInitialize()]
-//        public static void MyClassInitialize(TestContext testContext)
-//        {
-//            container = TestManager.ConfigureUnityContainer("unity");
-//            recommendationService = container.Resolve<IRecommendationService>();
-//            recommendationDao = container.Resolve<IRecommendationDao>();
-//            eventDao = container.Resolve<IEventDao>();
-//            usersGroupDao = container.Resolve<IUsersGroupDao>();
-//            categoryDao = container.Resolve<ICategoryDao>();
-//        }
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            container = TestManager.ConfigureUnityContainer("unity");
+            recommendationService = container.Resolve<IRecommendationService>();
+            recommendationDao = container.Resolve<IRecommendationDao>();
+            eventDao = container.Resolve<IEventDao>();
+            usersGroupDao = container.Resolve<IUsersGroupDao>();
+            categoryDao = container.Resolve<ICategoryDao>();
+        }
 
-//        [ClassCleanup()]
-//        public static void MyClassCleanup()
-//        {
-//            TestManager.ClearUnityContainer(container);
-//        }
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            TestManager.ClearUnityContainer(container);
+        }
 
-//        [TestInitialize()]
-//        public void MyTestInitialize()
-//        {
-//            transaction = new TransactionScope();
-//        }
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            transaction = new TransactionScope();
+        }
 
-//        [TestCleanup()]
-//        public void MyTestCleanup()
-//        {
-//            transaction.Dispose();
-//        }
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            transaction.Dispose();
+        }
 
-//        [TestMethod()]
-//        public void CreateRecommendation()
-//        {
-//            Category category1 = Category.CreateCategory(0,"Futbol");
-//            categoryDao.Create(category1);
+        [TestMethod()]
+        public void CreateRecommendation()
+        {
 
-//            DateTime date = new DateTime();
-//            byte[] dateBytes = BitConverter.GetBytes(date.Ticks);
+        }
 
-//            Event e1 = Event.CreateEvent(0,"Evento 1", dateBytes, "Evento de prueba 1", category1.id);
-//            eventDao.Create(e1);
+        [TestMethod()]
+        public void CreateRecommendationException()
+        {
+            
+        }
+    }
+}
 
-//            UsersGroup ug = UsersGroup.CreateUsersGroup(0,"Partidos de futbol","Grupo para partidos de futbol");
-//            usersGroupDao.Create(ug);
-
-//            String reText = "Este evento es la polla";
-//            long reId = recommendationService.Create(e1, ug, reText);
-
-//            Recommendation re = recommendationDao.Find(reId);
-
-//            Assert.AreEqual(re.id, reId);
-//            Assert.AreEqual(re.eventId, e1.id);
-//            Assert.AreEqual(re.usersGroupId, ug.id);
-//            Assert.AreEqual(re.text, reText);
-//        }
-//    }
-//}
