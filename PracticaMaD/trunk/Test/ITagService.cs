0e68dab67642ebf09291fbdc27c229381d47b186
alejandro.fernandez <alejandro.fernandez@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-12-04 02:16:19 +0000

    Añadida clase vacía para los nuevos test.
    Modificacion en los test de event en un comentario que era incorrecto.

---
+++ PracticaMaD/trunk/Test/ITagService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//TODO Hacer test.
+namespace Es.Udc.DotNet.PracticaMaD.Test
+{
+    /// <summary>
+    /// Descripción resumida de ITagService
+    /// </summary>
+    [TestClass]
+    public class ITagService
+    {
+        
+    }
+}
