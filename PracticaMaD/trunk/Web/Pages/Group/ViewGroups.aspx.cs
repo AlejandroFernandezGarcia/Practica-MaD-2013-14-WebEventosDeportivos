32ec0098bc5ce9265a763e4495cd233049714e3d
elias.grande <elias.grande@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-12-29 18:45:21 +0000

    Añadida página ViewGroups (ver todos los grupos y unirse a ellos).
    -- PracticaMaDEntities.edmx
    
    Regenerado desde DB porque me las estaba liando pardas.
    Acordaos de volver a ejecutar el script sql.
    
    --  Styles.css
    
    Nuevos estilos para la nueva página, los class "dataView" y "feedbackXXX" son reusables para otras páginas.
    
    -- ViewGroups.aspx/.cs/.resx/.designer.cs
    
    La nueva página para mostrar todos los grupos y unirse a ellos.
    
    -- Register.aspx/.designer.cs
    
    Corregidos errores de falta de claves en los ".resx" (resources), eran claves autogeneradas por el VS en el ".aspx" (plantilla) y no se usaban, ya que los asigna directamente el ".cs" (code behind).
    
    -- PracticaMaD.Master
    
    Ruta del link a ViewGroups bien puesta.
    
    -- Web.config
    
    Permitir el acceso a ViewGroups sin estar autenticado.

---
+++ PracticaMaD/trunk/Web/Pages/Group/ViewGroups.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.PracticaMaD.Model;
+using Es.Udc.DotNet.PracticaMaD.Model.UsersGroupService;
+using Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Group
+{
+    public partial class ViewGroups : System.Web.UI.Page
+    {
+        protected readonly IUsersGroupService UsersGroupService =
+            UnityResolver.Resolve<IUsersGroupService>();
+
+        protected List<long> MyGroupIdList { get; set; }
+
+        protected int GroupListCurrentRow { get; set; }
+        protected bool UserIsLogged { get; set; }
+        protected long UserProfileId { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // hide labels
+            lblUserAdded.Visible = false;
+            lblOperationFailed.Visible = false;
+
+            // initialize UserIsLogged & UserProfileId
+            UserSession userSession = SessionManager.GetUserSession(Context);
+            if (userSession == null)
+            {
+                UserProfileId = -1L;
+                UserIsLogged = false;
+            }
+            else
+            {
+                UserProfileId = userSession.UserProfileId;
+                UserIsLogged = true;
+            }
+
+            // populate GroupList repeater
+            if (!Page.IsPostBack)
+            {
+                MyGroupIdList = UserIsLogged ? GroupDtoListToIdList(UsersGroupService.FindByUserId(
+                    UserProfileId)) : new List<long>();
+                GroupList.DataSource = UsersGroupService.FindAllGroups();
+                GroupListCurrentRow = 0;
+                GroupList.DataBind();
+            }
+
+            // enable submit only if logged
+            btnJoinGroups.Enabled = UserIsLogged;
+            lblLoginRequired.Visible = !UserIsLogged;
+        }
+
+        protected void GroupList_OnItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            // if the data bound item is an item or alternating item (not the header etc)
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                UsersGroupDto item = (UsersGroupDto)e.Item.DataItem;
+                GroupListCurrentRow++;
+                if (GroupListCurrentRow % 2 == 0)
+                {
+                    TableRow tr = (TableRow)e.Item.FindControl("tr");
+                    tr.CssClass = "alt";
+                }
+
+                Label lblName = (Label)e.Item.FindControl("lblName");
+                lblName.Text = item.usersGroup.name;
+
+                Label lblRecomCount = (Label)e.Item.FindControl("lblRecomCount");
+                lblRecomCount.Text = item.numOfRecommendations.ToString();
+
+                Label lblUserCount = (Label)e.Item.FindControl("lblUserCount");
+                lblUserCount.Text = item.numOfUsers.ToString();
+
+                CheckBox cbJoin = (CheckBox)e.Item.FindControl("cbJoin");
+                cbJoin.Checked = MyGroupIdList.Contains(item.usersGroup.id);
+                cbJoin.Enabled = (UserIsLogged && !cbJoin.Checked);
+
+                HiddenField hfId = (HiddenField)e.Item.FindControl("hfId");
+                hfId.Value = item.usersGroup.id.ToString();
+            }
+        }
+
+        protected void BtnJoinGroupsClick(object sender, EventArgs e)
+        {
+            if (!UserIsLogged)
+            {
+                lblLoginRequired.Visible = true;
+                return;
+            }
+
+            List<long> usersGroupIds = new List<long>();
+            List<RepeaterItem> addedItems = new List<RepeaterItem>();
+
+            foreach (RepeaterItem item in GroupList.Items)
+            {
+                if (item.ItemType == ListItemType.Item
+                    || item.ItemType == ListItemType.AlternatingItem)
+                {
+                    CheckBox cbJoin = (CheckBox)item.FindControl("cbJoin");
+                    if (cbJoin.Checked && cbJoin.Enabled)
+                    {
+                        // remember id (to use the service later)
+                        HiddenField hfId = (HiddenField)item.FindControl("hfId");
+                        long id = Int64.Parse(hfId.Value);
+                        usersGroupIds.Add(id);
+
+                        // remember item (to update view after service use)
+                        addedItems.Add(item);
+                    }
+                }
+            }
+
+            if (usersGroupIds.Count > 0)
+            {
+                try
+                {
+                    // use service to add the user to the groups
+                    UsersGroupService.AddUserToGroup(usersGroupIds, UserProfileId);
+
+                    // show success feedback
+                    lblUserAdded.Visible = true;
+
+                    // update view
+                    foreach (RepeaterItem item in addedItems)
+                    {
+                        // disable checkbox
+                        ((CheckBox)item.FindControl("cbJoin")).Enabled = false;
+                        // increment user count
+                        Label lblUserCount = (Label)item.FindControl("lblUserCount");
+                        lblUserCount.Text = (Int64.Parse(lblUserCount.Text) + 1L).ToString();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    if (ex is DuplicateInstanceException || ex is UpdateException || ex is SqlException)
+                    {
+                        lblOperationFailed.Visible = true;
+                        return;
+                    }
+
+                    // yes, only "throw", not "throw ex", is the correct way
+                    // to avoid create a new exception with an empty stacktrace
+                    throw;
+                }
+            }
+        }
+
+        private List<long> GroupDtoListToIdList(List<UsersGroupDto> usersGroupDtoList)
+        {
+            List<long> list = new List<long>();
+
+            foreach (UsersGroupDto usersGroupDto in usersGroupDtoList)
+            {
+                list.Add(usersGroupDto.usersGroup.id);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ PracticaMaD/trunk/Web/Pages/Group/ViewGroups.aspx.designer.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Group {
+    
+    
+    public partial class ViewGroups {
+        
+        /// <summary>
+        /// Control viewGroupsForm.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm viewGroupsForm;
+        
+        /// <summary>
+        /// Control lblLoginRequired.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLoginRequired;
+        
+        /// <summary>
+        /// Control lblOperationFailed.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblOperationFailed;
+        
+        /// <summary>
+        /// Control lblUserAdded.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUserAdded;
+        
+        /// <summary>
+        /// Control lclName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclName;
+        
+        /// <summary>
+        /// Control lclUserCount.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclUserCount;
+        
+        /// <summary>
+        /// Control lclRecomCount.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclRecomCount;
+        
+        /// <summary>
+        /// Control lclJoin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Localize lclJoin;
+        
+        /// <summary>
+        /// Control GroupList.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater GroupList;
+        
+        /// <summary>
+        /// Control btnJoinGroups.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnJoinGroups;
+    }
+}
