ee4738830f1feeee93fad0a0c60bfc220a6cf453
elias.grande <elias.grande@5db608b4-c3ef-422f-8fe1-ae51119e584e>
2013-12-29 01:56:32 +0000

    Añadido test que faltaba tras la modificación del UsersGroupService.Create e implementada la inyección de dependencias de manera un poco menos cocha (ver linea 16 del CreateGroup.aspx.cs).

---
+++ PracticaMaD/trunk/Web/UnityResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Es.Udc.DotNet.PracticaMaD.Model.EventService;
+using Es.Udc.DotNet.PracticaMaD.Model.RecommendationService;
+using Es.Udc.DotNet.PracticaMaD.Model.TagService;
+using Es.Udc.DotNet.PracticaMaD.Model.UserService;
+using Es.Udc.DotNet.PracticaMaD.Model.UsersGroupService;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+
+namespace Es.Udc.DotNet.PracticaMaD.Web
+{
+    public static class UnityResolver
+    {
+        public static T Resolve<T>()
+        {
+            var container = (IUnityContainer)HttpContext.Current.Application["unityContainer"];
+            return container.Resolve<T>();
+        }
+    }
+}
